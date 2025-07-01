4cbc3850b1f904966bee6572285a0ea328d61252
LoweKTH <62517085+LoweKTH@users.noreply.github.com>
2025-07-01 16:27:29 +0200

    Base structure for project

---
+++ backend/src/main/java/com/exjobb/backend/MarketingAgentFactoryV2Application.java
@@ -0,0 +1,13 @@
+package com.exjobb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingAgentFactoryV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingAgentFactoryV2Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/exjobb/backend/MarketingAgentFactoryV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exjobb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingAgentFactoryV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
